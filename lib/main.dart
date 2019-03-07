f925dd14f104b564ddd4bc99884a7bd35fbe0dfc
SeniorZhai <developer.zhaitao@gmail.com>
2019-03-07 20:05:41 +0800

    import libary

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 // 创建并运行APP
 void main() => runApp(new MyApp());
@@ -7,6 +8,7 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
         title: 'Welcome to Flutter',
         home: new Scaffold(
@@ -14,6 +16,6 @@ class MyApp extends StatelessWidget {
             appBar: new AppBar(
               title: new Text('Welcome to Flutter'),
             ),
-            body: new Center(child: new Text('Hello World'))));
+            body: new Center(child: new Text(wordPair.asPascalCase))));
   }
 }
