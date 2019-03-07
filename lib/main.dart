f8608f7e63eb29806af018ee6b013fe6a5def965
SeniorZhai <developer.zhaitao@gmail.com>
2019-03-07 20:21:38 +0800

    infinite list

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Welcome to Flutter',
-        home: new Scaffold(
-            // Scaffold 是 Material library 提供的一个widget 提供默认的导航栏、标题、包含主屏幕的widget树
-            appBar: new AppBar(
-              title: new Text('Welcome to Flutter'),
-            ),
-            body: new Center(child: new RandomWords())));
+        title: 'Welcome to Flutter', home: new RandomWords());
   }
 }
 
@@ -28,9 +22,32 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(title: new Text(pair.asPascalCase, style: _biggerFont));
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return new Scaffold(
+        appBar: new AppBar(title: new Text('Startup Name Generator')),
+        body: _buildSuggestions());
   }
 }
